c668e8dde0d10da8e1478bc5e0344555f1885b7e
Phan Anh <phananh12042003@gmail.com>
2023-07-12 04:03:29 -0400

    search by tags and title

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -2,8 +2,10 @@ import express from "express";
 import {
   addVideo,
   addView,
+  getByTag,
   getVideo,
   random,
+  search,
   sub,
   trend,
 } from "../controllers/video.js";
@@ -13,12 +15,22 @@ const router = express.Router();
 
 //create a video
 router.post("/", veirifyToken, addVideo);
+//update video
 router.put("/:id", veirifyToken, addVideo);
+//delete video
 router.delete("/", veirifyToken, addVideo);
+//get video by ID
 router.get("/find/:id", getVideo);
+//get view
 router.put("/view/:id", addView);
+//get trend video
 router.get("/trend", trend);
+//get random video
 router.get("/random", random);
 //subscribed channel videos
 router.get("/sub", veirifyToken, sub);
+//find video by tags
+router.get("/tags", getByTag);
+//find movie by title
+router.get("/search", search);
 export default router;
